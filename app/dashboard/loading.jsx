a1df1104546653483c25ada8bf0be8cafefe1859
darshanrijal <darshanrijal0@gmail.com>
2024-06-13 14:11:39 +0545

    Deleted loading.jsx and page.jsx files from app/dashboard directory

--- app/dashboard/loading.jsx
+++
@@ -1,8 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton";
-import React from "react";
-
-const Loading = () => {
-  return <Skeleton className="w-60 h-96 rounded-md" />;
-};
-
-export default Loading;
