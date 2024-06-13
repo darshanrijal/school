c6b6b3d26b8c0dcd0f1436b52113e4fbd113b79e
darshanrijal <darshanrijal0@gmail.com>
2024-06-13 13:27:02 +0545

    "Update NextTopLoader props in RootLayout"

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <NextTopLoader />
+        <NextTopLoader showSpinner={false} color="#708090" />
         <Navbar />
         {children}
         <Toaster />
