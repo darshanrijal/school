8c8b1d645cf1629ed8cfbc73c4b4ea2e44474a78
darshanrijal <darshanrijal0@gmail.com>
2024-06-14 21:13:07 +0545

    Removed StudentDetailsForm import and component, replaced with welcome message and navigation instruction

--- app/(notadmin)/page.jsx
+++ app/(notadmin)/page.jsx
@@ -1,9 +1,10 @@
-import StudentDetailsForm from "@/components/StudentDetailsForm";
-
 const Home = () => {
   return (
-    <div className="mt-6">
-      <StudentDetailsForm />
+    <div className="flex justify-center items-center flex-col my-48">
+      <div>Welcome to school app portal</div>
+      <div className="md:hiddm">
+        <code>see the naviagtion menu for more</code>
+      </div>
     </div>
   );
 };
