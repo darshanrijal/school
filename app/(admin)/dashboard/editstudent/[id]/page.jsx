8251adff376687a7b3531efb5c0ccc7eab42ccc3
darshanrijal <darshanrijal0@gmail.com>
2024-06-15 12:20:36 +0545

    Updated EditStudent page to use NEXT_PUBLIC_API_BASE_URL and added error handling for student data fetch

--- app/(admin)/dashboard/editstudent/[id]/page.jsx
+++ app/(admin)/dashboard/editstudent/[id]/page.jsx
@@ -1,22 +1,38 @@
 import EditStudent from "@/components/EditStudentForm";
 import React from "react";
+
 const getStudentbyID = async (id) => {
   try {
-    const res = await fetch(`http://localhost:3000/api/students/${id}`, {
-      cache: "no-store",
-    });
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/students/${id}`,
+      { cache: "no-store" }
+    );
     if (!res.ok) {
       throw new Error("Failed to fetch");
     }
-    return res.json();
+    const data = await res.json();
+    return data;
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    return null;
   }
 };
+
 const page = async ({ params }) => {
   const { id } = params;
-  const { student } = await getStudentbyID(id);
+  const data = await getStudentbyID(id);
+
+  if (!data || !data.student) {
+    return (
+      <div className="grid place-items-center mt-40">
+        <code>Error fetching student data or student not found.</code>
+      </div>
+    );
+  }
+
+  const { student } = data;
   const { name, roll } = student;
+
   return (
     <div>
       <EditStudent id={id} name={name} roll={roll} />
