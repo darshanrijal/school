eaec25f21b2239a6cecd562f9694443a9f28065d
darshanrijal <darshanrijal0@gmail.com>
2024-06-15 12:28:32 +0545

    Updated Home component: changed div class from 'md:hiddm' to 'grid place-items-center', added two new <code> elements and a <br /> element.

--- app/(notadmin)/page.jsx
+++ app/(notadmin)/page.jsx
@@ -2,8 +2,10 @@ const Home = () => {
   return (
     <div className="flex justify-center items-center flex-col my-48">
       <div>Welcome to school app portal</div>
-      <div className="md:hiddm">
+      <div className="grid place-items-center">
         <code>see the naviagtion menu for more</code>
+        <br />
+        <code>Checkpoint:Authenticaion</code>
       </div>
     </div>
   );
