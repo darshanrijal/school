405689a8be33ef49c88f6920c998982202364a39
darshanrijal <darshanrijal0@gmail.com>
2024-06-17 13:23:33 +0545

    Added Button and Link imports, and a refresh button to MessageLayout component

--- app/(admin)/dashboard/messages/layout.jsx
+++ app/(admin)/dashboard/messages/layout.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { roboto } from "@/components/ui/fonts";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export const metadata = {
   title: "Messages",
 };
@@ -7,7 +9,10 @@ export default function MessageLayout({ children }) {
   return (
     <div>
       <h1 className={`${roboto.className} text-center font-medium  `}>
-        Messages List
+        Messages List{" "}
+        <Button variant="outline">
+          <a href={""}>Refresh</a>
+        </Button>
       </h1>
       {children}
     </div>
