e019459eaf2998a9e0d5bd585ea1b4d709c4455f
darshanrijal <darshanrijal0@gmail.com>
2024-06-17 17:01:33 +0545

    Updated MessageLayout component style

--- app/(admin)/dashboard/messages/layout.jsx
+++ app/(admin)/dashboard/messages/layout.jsx
@@ -3,14 +3,14 @@ import { roboto } from "@/components/ui/fonts";
 import { Button } from "@/components/ui/button";
 export default function MessageLayout({ children }) {
   return (
-    <div>
+    <div className="flex flex-col gap-5">
       <h1 className={`${roboto.className} text-center font-medium  `}>
         Messages List{" "}
         <a href={""}>
           <Button variant="outline">Refresh</Button>
         </a>
       </h1>
-      {children}
+      <div>{children}</div>
     </div>
   );
 }
